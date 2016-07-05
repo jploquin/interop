3500898fc2045744504ddd186f15cfbc5bd95bd7
Ubuntu <ubuntu@ip-172-31-22-253.us-west-2.compute.internal>
2016-07-05 12:49:30 +0000

    externalise password

--- postgresql/create-dataonly-container.sh
+++ postgresql/create-dataonly-container.sh
@@ -1,3 +1,9 @@
-# create data container using the postgres image, give it a name so you can access it
-sudo docker run --name pgdata interop-postgres -e POSTGRES_PASSWORD=figaront echo "data only"
-# it is now important to never run `docker rm pgdata` (not that it looks like a command you should run)
+if test -z "$1"
+then
+  echo 'please enter a password as parameter'
+else
+
+  # create data container using the postgres image, give it a name so you can access it
+  sudo docker run --name pgdata interop-postgres -e POSTGRES_PASSWORD=$1 echo "data only"
+  # it is now important to never run `docker rm pgdata` (not that it looks like a command you should run)
+fi
